ba543af892245c88177e9c361b964c98d8dabd38
Jeff Hann <jeffhann@gmail.com>
2014-04-28 14:14:49 -0300

    innitial commit

---
+++ src/com/swear/SwearWidgetProvider.java
@@ -0,0 +1,14 @@
+package com.swear;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+
+
+public class SwearWidgetProvider extends AppWidgetProvider {
+
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+    }
+ 
+}
