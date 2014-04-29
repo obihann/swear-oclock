b9e1d15af1b7c3d27ea31c88d61e1dbb42a1865f
Jeff Hann <jeffhann@gmail.com>
2014-04-29 20:22:41 -0300

    updating layouts for keyguard and regular, also adding a initial screen

--- src/com/obihann/swear/ShittyBroadcastReceiver.java
+++ src/com/obihann/swear/ShittyBroadcastReceiver.java
@@ -7,10 +7,12 @@ import java.util.Locale;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProviderInfo;
 import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 import android.os.PowerManager;
 import android.widget.RemoteViews;
 
@@ -27,30 +29,39 @@ public class ShittyBroadcastReceiver extends BroadcastReceiver {
 		wl.acquire();
 
 		try {
-			//You can do the processing here update the widget/remote views.
-			RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.sluty_layout);
-	
-			Date now = new Date();
-			SimpleDateFormat hourFormat = new SimpleDateFormat("H", Locale.CANADA);
-			SimpleDateFormat minuteFormat = new SimpleDateFormat("m", Locale.CANADA);
-	
-			int hour = Integer.parseInt(hourFormat.format(now));
-			int minute = Integer.parseInt(minuteFormat.format(now));
+			ComponentName thisWidget = new ComponentName(context, FuckingWidgetProvider.class);
+			AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
 			
-			if (hour > 12) {
-				hour = hour - 12;
+			for (int widgetId : appWidgetManager.getAppWidgetIds(thisWidget)) {
+				Bundle myOptions = appWidgetManager.getAppWidgetOptions(widgetId);
+				
+				int category = myOptions.getInt(AppWidgetManager.OPTION_APPWIDGET_HOST_CATEGORY, -1);
+				boolean isKeyguard = category == AppWidgetProviderInfo.WIDGET_CATEGORY_KEYGUARD;
+				int baseLayout = isKeyguard ? R.layout.sluty_layout_lock : R.layout.sluty_layout;
+				
+				//Get the remote views
+				//You can do the processing here update the widget/remote views.
+				RemoteViews remoteViews = new RemoteViews(context.getPackageName(), baseLayout);
+		
+				Date now = new Date();
+				SimpleDateFormat hourFormat = new SimpleDateFormat("H", Locale.CANADA);
+				SimpleDateFormat minuteFormat = new SimpleDateFormat("m", Locale.CANADA);
+		
+				int hour = Integer.parseInt(hourFormat.format(now));
+				int minute = Integer.parseInt(minuteFormat.format(now));
+				
+				if (hour > 12) {
+					hour = hour - 12;
+				}
+				
+				if (hour == 0) {
+					hour = 12;
+				}
+		
+				remoteViews.setTextViewText(R.id.hourTextView, EnglishNumberToWords.convert(hour));
+				remoteViews.setTextViewText(R.id.minuteTextView, EnglishNumberToWords.convert(minute));
+				appWidgetManager.updateAppWidget(thisWidget, remoteViews);
 			}
-			
-			if (hour == 0) {
-				hour = 12;
-			}
-	
-			remoteViews.setTextViewText(R.id.hourTextView, EnglishNumberToWords.convert(hour));
-			remoteViews.setTextViewText(R.id.minuteTextView, EnglishNumberToWords.convert(minute));
-			
-			ComponentName thiswidget = new ComponentName(context, FuckingWidgetProvider.class);
-			AppWidgetManager manager = AppWidgetManager.getInstance(context);
-			manager.updateAppWidget(thiswidget, remoteViews);
 		}
 		finally {
 			//Release the lock
